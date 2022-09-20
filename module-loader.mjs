d0f6dbc982de238defb2d1c30e5259a0bd24d0a0
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2022-09-20 08:54:57 -0700

    fix bug

--- module-loader.mjs
+++ module-loader.mjs
@@ -1,7 +1,7 @@
 export function resolve(specifier, context, defaultResolve) {
     // rewrite all imports that start with @my_workspace/ to be relative
     if (specifier.startsWith('@my_workspace/')) {
-        specifier = specifier.replace('@my_workspace/', './');
+        specifier = specifier.replace('@my_workspace/', process.env.PWD + "/");
     }
 
     // Let Node.js handle all other specifiers, such as package names
